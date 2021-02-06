08debc14854e78972ba294a217ffc8d3cdf884d3
nunsinee <71260513+nunsinee@users.noreply.github.com>
2021-02-06 08:26:00 +0100

    Updated answer

--- js/question-1.js
+++ js/question-1.js
@@ -1,4 +1,4 @@
 //Question 1
 //Answer..
-//use let for this vairable can reassign
+//use let for this vairable can reassign later.
 let division = (a, b) => a % b;
--- js/question-2.js
+++ js/question-2.js
@@ -3,7 +3,6 @@
 
 const url =
 	"https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
-
 const resultsContainer = document.querySelector(".results");
 
 const createHTML = (results) => {
